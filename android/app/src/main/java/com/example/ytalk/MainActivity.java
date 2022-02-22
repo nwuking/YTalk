0753d7d417323f0862536ec59f4ea6cf5debfb77
nwuking_wind <2283300457@qq.com>
2022-02-22 22:08:53 +0800

    init application

--- android/app/src/main/java/com/example/ytalk/MainActivity.java
+++ android/app/src/main/java/com/example/ytalk/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.ytalk;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ytalk.activities.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
---
+++ android/app/src/main/java/com/example/ytalk/db/ChatDb.java
@@ -0,0 +1,29 @@
+package com.example.ytalk.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 用于操作存储聊天消息的类
+ */
+public class ChatDb extends SQLiteOpenHelper {
+    private Context context;
+
+    public ChatDb(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
